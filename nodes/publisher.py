1a2a70bd0ce16addc4996ccf59a156530c7dd7d5
Buraq Khan <buraqkhan18@gmail.com>
2024-09-17 14:13:31 +0300

    Shift practice 1 to new repo

---
+++ nodes/publisher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node('publisher')
+rate = rospy.Rate(rospy.get_param('~rate', 2))
+pub = rospy.Publisher('/message', String, queue_size=10)
+message = rospy.get_param('~message', 'Hello World!')
+
+while not rospy.is_shutdown():
+    pub.publish(message)
+    rate.sleep()
